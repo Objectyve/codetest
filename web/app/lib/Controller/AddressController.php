1976f7f62e42ce71cef648505213e2a9399effb7
= <=>
2015-08-03 11:16:28 -0700

    Laying ground work for caching mechanism

--- web/app/lib/Controller/AddressController.php
+++ web/app/lib/Controller/AddressController.php
@@ -32,52 +32,94 @@ class AddressController
     {
         $payload = json_decode($request->getContent());;
 
+        $concat_input = $payload->street.",".$payload->city.",".$payload->state;
+
         $geocoder = $app['geocoder'];
 
-        $process = true;
+        $process_api = true;
 
-        try {
-            $result   = $geocoder->geocode($payload->street.",".$payload->city.",".$payload->state);
-        } catch (\Geocoder\Exception\NoResultException $e) {
-            $result = "No result was returned by the geocoder, the address appears to be malformed";
-            //print_r($result); die();
-            $process = false;
-        } catch (\Geocoder\Exception\QuotaExceededException $e) {
-            $result = "We met our daily quota";
-            $process = false;
-        } catch (Exception $e) {
-            $result = "Error: " . $e->getMessage();
-            $process = false;
-        }
 
+
+
+        // clean old entries out of the database
+        //$app['db']->delete('address_geocoding_json WHERE data_added!= CURDATE()');
+
+        // check the database to see if the json has been cached today
+        $cache_check = $app['db']->fetchAssoc("SELECT json FROM address_geocoding_json WHERE address=:ADDRESS", ['ADDRESS' => $concat_input]);
+
+        //print_r($check); die();
         /*
-        echo "Street: " . $result->getStreetNumber() . " ".$result->getStreetName()."\n";
-        echo "City: " . $result->getCity() . "\n";
-        echo "State: " . $result->getRegion() . "\n";
-        echo "Zip: " . $result->getZipCode() . "\n";
+        $resource = [
+            'error' => $check
+        ];
         */
+        //return new JsonResponse($resource);
 
-        #print_r($result); exit;
 
-        // AIzaSyBzM7VfVA8uhFxLnVDlwpUFJzaJBhLEtn0
+        if (!$cache_check) { // if not cached, fetch the json from Google
 
-        if ($process) {
-            $newResource = [
-                'id'      => (integer)$app['db']
-                        ->fetchColumn("SELECT max(id) FROM address") + 1,
-                'street'  => $result->getStreetNumber() . " ".$result->getStreetName(),
-                'city' => $result->getCity(),
-                'state' => $result->getRegion(),
-                'postalcode' => $result->getZipCode(),
-            ];
+            // cache in the database for future use
 
-            $app['db']->insert('address', $newResource);
+            try {
+                $result = $geocoder->geocode($payload->street . "," . $payload->city . "," . $payload->state);
+            } catch (\Geocoder\Exception\NoResultException $e) {
+                $result = "No result was returned by the geocoder, the address appears to be malformed";
+                //print_r($result); die();
+                $process_api = false;
+            } catch (\Geocoder\Exception\QuotaExceededException $e) {
+                $result = "We met our daily quota";
+                $process_api = false;
+            } catch (Exception $e) {
+                $result = "Error: " . $e->getMessage();
+                $process_api = false;
+            }
+
+
+            #print_r($result); exit;
+
+            // AIzaSyBzM7VfVA8uhFxLnVDlwpUFJzaJBhLEtn0
+
+            if ($process_api) {
+                $newResource = [
+                    'id' => (integer)$app['db']
+                            ->fetchColumn("SELECT max(id) FROM address") + 1,
+                    'street' => $result->getStreetNumber() . " " . $result->getStreetName(),
+                    'city' => $result->getCity(),
+                    'state' => $result->getRegion(),
+                    'postalcode' => $result->getZipCode(),
+                ];
+
+                $app['db']->insert('address', $newResource);
+
+                if ($_SERVER['REMOTE_ADDR'] == '71.160.4.106') {
+
+                    // insert into cache
+                    $newCacheResource = [
+                        'address' => $concat_input,
+                        'json' => '', //$result->getJson(),
+                        'date_added' => date('Y-m-d h:i:s') // 'NOW()',
+
+                    ];
+
+                    //$sql2 = "INSERT INTO address_geocoding_json (address,json,date_added) VALUES (?,?,NOW())";
+                    $app['db']->insert('address_geocoding_json', $newCacheResource);
+                }
+
+
+            } else {
+                // ap error
+                $newResource = [
+                    'error' => $result
+                ];
+            }
 
         } else {
+            // address already cached
             $newResource = [
-                'error' => $result
+                'error' => 'cached'
             ];
         }
+
         return new JsonResponse($newResource);
     }
 
@@ -87,23 +129,23 @@ class AddressController
 
         $geocoder = $app['geocoder'];
 
-        $process = true;
+        $process_api = true;
 
         try {
             $result   = $geocoder->geocode($payload->street.",".$payload->city.",".$payload->state);
         } catch (\Geocoder\Exception\NoResultException $e) {
             $result = "No result was returned by the geocoder, the address appears to be malformed";
             //print_r($result); die();
-            $process = false;
+            $process_api = false;
         } catch (\Geocoder\Exception\QuotaExceededException $e) {
             $result = "We met our daily quota";
-            $process = false;
+            $process_api = false;
         } catch (Exception $e) {
             $result = "Error: " . $e->getMessage();
-            $process = false;
+            $process_api = false;
         }
 
-        if ($process) {
+        if ($process_api) {
             $resource = [
                 'street'  => $result->getStreetNumber() . " ".$result->getStreetName(),
                 'city' => $result->getCity(),
